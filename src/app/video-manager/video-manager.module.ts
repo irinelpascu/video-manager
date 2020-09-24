2de473d4292e71a413c2f081c9975fb975c5aa83
irinelpascu <irinel.pascu@toptal.com>
2020-09-24 22:39:12 +0300

    Create video-manager Angular app. Create video-manager.module with video-manager.component. Install Angular Material and configure color palettes. Install NGRX.

---
+++ src/app/video-manager/video-manager.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoManagerComponent } from './containers/video-manager/video-manager.component';
+
+
+
+@NgModule({
+  declarations: [VideoManagerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class VideoManagerModule { }
